01916515d4b03413c44ae935f1fc21f97d72dd88
Tatsiana82 <upsen2020@gmail.com>
2023-09-16 13:30:23 +0300

    16 sept

--- train.py
+++ train.py
@@ -1,2 +1,14 @@
-print('hello')
-print('Yes')
+#task 1
+my_dict = {
+    'title': 'Three pigs',
+    'main_actor': 'Nif_Nif',
+    'slogan': 'Ne strojte dom na peske'
+}
+#task 2
+new_dict = {
+    'author': 'Sergey_Mihalkov',
+    'second_actor': 'Naf_Naf'
+}
+#task 3
+
+print(my_dict | new_dict)
